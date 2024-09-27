a956b1ce510c3253f5fae0197f4d2fffb39fba55
LoganOReed <me@loganreed.org>
2024-09-27 06:10:55 -0400

    rename

---
+++ python-sci/app/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def main():
+    """docstring for main"""
+    x = np.linspace(0,1,100)
+    print(x)
+    print("What")
+    
+
+if __name__ == "__main__":
+    print("Hello")
---
+++ python-sci/flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            poetry2nix.overlays.default
+          ];
+        };
+        python = pkgs.python39;
+        overrides = pkgs.poetry2nix.overrides.withDefaults (final: prev: {
+          # Python dependency overrides go here
+
+        });
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+
+          myapp = mkPoetryApplication { 
+              projectDir = self; 
+              python = python;
+              overrides = overrides;
+            };
+          default = self.packages.${system}.myapp;
+        };
+
+        # Shell for app dependencies.
+        #
+        #     nix develop
+        #
+        # Use this shell for developing your app.
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.myapp ];
+        };
+
+        # Shell for poetry.
+        #
+        #     nix develop .#poetry
+        #
+        # Use this shell for changes to pyproject.toml and poetry.lock.
+        devShells.poetry = pkgs.mkShell {
+          packages = [ pkgs.python39 pkgs.poetry ];
+        };
+      });
+}
